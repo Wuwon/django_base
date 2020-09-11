ee6434faefbe8bda572a16066fc7d71fac1449b5
unknown <wuny511@likelion.org>
2020-09-11 15:07:58 +0900

    CRUD end

--- startproject/postsapp/views.py
+++ startproject/postsapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from django.utils import timezone
 # Create your views here.
 
 def index(request):
@@ -13,10 +14,47 @@ def index(request):
 
 
 def detail(request,post_id):
-    post = Post.objects.get(id=post_id)
+    post = Post.objects.get(pk=post_id)
 
     context={
         'post' : post
     }
 
     return render(request,'detail.html',context)
+
+def new(request):
+    return render(request,'new.html')
+
+def create(request):
+    author = request.POST['author']
+    subject = request.POST['subject']
+    body = request.POST['body']
+    image = None
+
+    post = Post(author=author,subject=subject,body=body,image=image,create_time=timezone.now())
+    post.save()
+    
+    return redirect('detail', post_id=post.pk)
+
+def edit(request,post_id):
+    post = Post.objects.get(pk=post_id)
+    context = {
+        'post' : post
+    }
+    return render(request,'edit.html',context)
+
+
+def update(request,post_id):
+        post = Post.objects.get(pk=post_id)
+        subject = request.POST["subject"]
+        body = request.POST['body']
+        image = None
+        post = Post(subject=subject,body=body,image=image,create_time=timezone.now())
+        post.save()
+
+        return redirect('detail', post_id=post.pk)
+
+def delete(request,post_id):
+    post = Post.objects.get(pk=post_id)
+    post.delete()
+    return redirect('index')
--- startproject/startproject/urls.py
+++ startproject/startproject/urls.py
@@ -20,5 +20,10 @@ from postsapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('<int:post_id>/',views.detail, name='detail'),
+    path('<int:post_id>/', views.detail, name='detail'),
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name="create"),
+    path('<int:post_id>/edit/', views.edit, name='edit'),
+    path('<int:post_id>/update/', views.update, name='update'),
+    path('<int:post_id>/delete/',views.delete, name='delete'),
 ]
